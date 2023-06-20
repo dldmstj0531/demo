566409054efd4b81d2de7dcaacb2fb4dc8646bdf
Lee Eun Seo <dldmstj0531@gmail.com>
2023-06-20 16:47:58 +0900

    17. @Repository에서 쿼리 추가하기

--- src/main/java/com/example/demo/TodoService.java
+++ src/main/java/com/example/demo/TodoService.java
@@ -8,13 +8,12 @@ import com.example.demo.TodoRepository;
 public class TodoService {
 @Autowired
 private TodoRepository repository;
-public String testService(){
-// Todo Entity 생성
-TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
-// Todo Entity 저장
-repository.save(entity);
-// Todo Entity 검색
-TodoEntity savedEntity = repository.findById(entity.getId()).get();
-return savedEntity.getTitle();
-}
-}
+public String testService() {
+	
+	TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
+	repository.save(entity);
+	TodoEntity savedEntity = 
+	repository.findByUserId(entity.getUserId()).get(0);
+	return savedEntity.getUserId();
+	}
+	}
