cb494d0d143adb6244ebeae50183b9e72dcc725e
Lee Eun Seo <dldmstj0531@gmail.com>
2023-06-20 16:53:54 +0900

    18. @NamedQuery, @Query 사용하기

--- src/main/java/com/example/demo/TodoRepository.java
+++ src/main/java/com/example/demo/TodoRepository.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.example.demo.*;
 @Repository
 public interface TodoRepository extends JpaRepository<TodoEntity,String>{
-List<TodoEntity>findByUserId(String userId);
+@Query("select t from TodoEntity t where t.userId = ?1")
+List<TodoEntity>searchByUserId(String userId);
 }
--- src/main/java/com/example/demo/TodoService.java
+++ src/main/java/com/example/demo/TodoService.java
@@ -8,12 +8,11 @@ import com.example.demo.TodoRepository;
 public class TodoService {
 @Autowired
 private TodoRepository repository;
-public String testService() {
-	
-	TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
-	repository.save(entity);
-	TodoEntity savedEntity = 
-	repository.findByUserId(entity.getUserId()).get(0);
-	return savedEntity.getUserId();
-	}
-	}
+public String testService(){
+TodoEntity entity = TodoEntity.builder().userId("user01").title("My first todo item").build();
+repository.save(entity);
+TodoEntity savedEntity = 
+repository.searchByUserId(entity.getUserId()).get(0);
+return savedEntity.getUserId();
+}
+}
