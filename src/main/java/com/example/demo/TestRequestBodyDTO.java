c9633650c660ee8f41854886a8f0da4f5962b7d6
Lee Eun Seo <dldmstj0531@gmail.com>
2023-06-20 16:24:39 +0900

    12. @RequestBody 사용하기

---
+++ src/main/java/com/example/demo/TestRequestBodyDTO.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class TestRequestBodyDTO {
+
+private int id;
+public int getId() {
+	return (Integer)null;
+}
+private String message;
+public String getMessage() {
+// TODO Auto-generated method stub
+return null;
+}
+}
